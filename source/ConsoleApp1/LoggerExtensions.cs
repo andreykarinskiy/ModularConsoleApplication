e8ac2426acd11ab859a9c461711a9d9c183ad876
andrey.karinskiy <Andrey.Karinski@nexign.com>
2020-05-22 23:23:11 +0300

    Initial commit.

---
+++ source/ConsoleApp1/LoggerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+using Serilog.Context;
+
+namespace ConsoleApp1
+{
+    public static class LoggerExtensions
+    {
+        public static void Audith(this ILogger logger, AuditEvent auditEvent)
+        {
+            using (LogContext.PushProperty("EventId", auditEvent.EventId))
+            {
+                logger.Information(auditEvent.ToString());
+            }
+        }
+    }
+}
---
+++ source/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using Serilog;
+using Serilog.Context;
+using System;
+using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cefPattern = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} host CEF:0|Nexign|VideoanAlyst|1.0|{EventId}|{Message:lj}|{Level:u3}|{NewLine}";
+
+            var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate: cefPattern)
+                .WriteTo.File("c:\\temp\\cef.log", outputTemplate: cefPattern)
+                .CreateLogger();
+
+            Log.Logger = logger;
+
+
+
+            // Запись события аудита.
+            logger.Audith(new UserLoggedIn("123"));
+            
+            //using (LogContext.PushProperty("EventId", AuditableOperation.UserLoggedIn))
+            //{
+            //    Log.Information("Оператор id:{UserId} зашел в систему.", 12345);
+            //}
+
+            Log.CloseAndFlush();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class AuditEvent
+    {
+        protected AuditEvent(int eventId, string userId)
+        {
+            EventId = eventId;
+
+            UserId = userId;
+        }
+
+        public int EventId { get; }
+
+        public string UserId { get; }
+
+        protected abstract string Format { get; }
+
+        public override string ToString()
+        {
+            return Format;
+        }
+    }
+
+    public sealed class UserLoggedIn : AuditEvent
+    {
+        public UserLoggedIn(string userId) : base(eventId: 100, userId)
+        {
+        }
+
+        protected override string Format => $"Оператор id:{UserId} зашел в систему.";
+    }
+}
